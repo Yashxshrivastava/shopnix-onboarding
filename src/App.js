6276bdf8889e48b847f9d4bfe7ea7570ed285fb7
Yash Shrivastava <96059570+Yashxshrivastava@users.noreply.github.com>
2025-07-12 16:59:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // File: App.js
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
